57deb2142fc05f00afc2fe052682b07fd7300553
Anomis1998 <101574768+Anomis1998@users.noreply.github.com>
2023-03-08 17:50:42 +0100

    Add files via upload

---
+++ main-test.py
@@ -0,0 +1,9 @@
+# Importa la funzione Aggiungi e afferma che funziona correttamente. 
+dall'importazione principale Aggiungi _
+
+def TestAdd():
+         assert Add(2,3) == 5
+        print( "Aggiungi funzione funziona correttamente" )
+
+if __name__ == '__main__' :
+        ProvaAggiungi()
---
+++ main.py
@@ -0,0 +1,8 @@
+def Add(a, b):
+         restituisce a + b
+        
+def Saluta():
+        print( "sup mondo da srcmake" )
+
+if __name__ == '__main__' :
+        Di Ciao()
